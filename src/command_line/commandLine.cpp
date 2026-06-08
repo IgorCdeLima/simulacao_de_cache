d03026a3588ed87e8b6f84ca2a59a08b84dd49c1
Igor Cechinato de Lima <156817223+IgorCdeLima@users.noreply.github.com>
2026-06-08 17:30:59 -0300

    v_1.2

--- src/command_line/commandLine.cpp
+++ src/command_line/commandLine.cpp
@@ -1,15 +1,42 @@
 #include <commandLine.h>
 #include <stdio.h>
+#include <string>
+
+
+#define NUMBER_OF_ARGUMENTS  9
+#define POSITION_NAME_APPLICATION 0
+#define ENTRY_POLICE 1
+#define SIZE_LINE 2
+#define NUMBER_OF_LINES 3
+#define ASSOCIABILITY 4
+#define ACESS_TIME 5
+#define SUBSTITUTION_POLICY 6
+#define TIME_TO_READ_WRITE 7
 
 // commandLine::commandLine(/* args */){}
 
 void commandLine::argumentsForClass(int argc, char *argv[]){
-    int POSICAO_DE_ARGUMENTO = 0;
-    printf("Total de argumentos: %d\n", argc);
+    int positionToRealArguments;
 
-    for(POSICAO_DE_ARGUMENTO; POSICAO_DE_ARGUMENTO < argc; POSICAO_DE_ARGUMENTO++ ){
-        printf("Argumento %d: %s\n", POSICAO_DE_ARGUMENTO, argv[POSICAO_DE_ARGUMENTO] );
+    // if not have 8 arguments, return and inform the user how to use the correct command line
+    if(argc != NUMBER_OF_ARGUMENTS ){
+        printf("Usage: %s <entry_policy> <size_line> <number_of_lines> <associability> <access_time_per_hit> <substituition_policy> <time_to_read_write>\n", argv[POSITION_NAME_APPLICATION]);
+        return;
     }
+
+    // variables to receive arguments from command line
+    int entryPolicy  = std::stoi(argv[ENTRY_POLICE]);
+    int sizeLine = std::stoi(argv[SIZE_LINE]);
+    int numberLines = std::stoi(argv[NUMBER_OF_LINES]);
+    int associability = std::stoi(argv[ASSOCIABILITY]);
+    int accessTimePerHit = std::stoi(argv[ACESS_TIME]);
+    std::string substitutionPolicy = argv[SUBSTITUTION_POLICY];
+    int timeToReadWrite = std::stoi(argv[TIME_TO_READ_WRITE]);
+    
+
+    // for(positionToRealArguments = 1; positionToRealArguments < argc; positionToRealArguments++ ){
+
+    // }
 }
 
 
